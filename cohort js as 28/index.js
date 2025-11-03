7bebc05e1741cbeba6ca676273f72a573ad6b151
Harsh Prajapati <harshcode26@gmail.com>
2025-11-04 01:00:06 +0530

    first commit

---
+++ cohort js as 28/index.js
@@ -0,0 +1,111 @@
+// console.log("connected");
+
+// document.title
+// 'Document'
+// alert("heloo")
+
+//js is brain
+
+//i have used it in fetching api data, animation (gsap), etc
+
+// var a = "harsh";
+
+// let b = "sheriyans";
+
+// const c= "school";
+
+// a = "anand";
+// b = "sheriyans coding";
+
+// // c = "college"; --> leads to type error
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+
+// {
+//     let myName = "harsh";
+// }
+
+// console.log(myName);
+
+//const can be used in PI, PF Percentage, and in baseUrl(mostly api keys)
+
+// let username = prompt("enter user name: ");
+
+// alert(`welcome, ${username}`);
+
+// console.log(typeof username);
+
+// console.log(username);
+// console.log(20);
+// console.log("gandhidham");
+
+// console.info(username)
+// console.info(20);
+// console.info("gandhidham");
+
+// console.warn("harsh")
+// console.warn(20);
+// console.warn("gandhidham");
+
+// let age = prompt("age: ");
+
+// age = Number(age);
+
+// console.log(age + 5);
+
+
+// let msg = "i love her"; console.log(msg.slice(2,6))
+
+// msg.slice(2,6);
+
+
+// console.log("harsh")
+// console.log(20)
+// console.log("gandhidham")
+
+/*so this code prompts age from user means it take input from user and then converts it into  number then while printing it in console it adds +5 in age.*/
+
+// let y = (5+10)*2;
+
+// console.log(y);
+
+//An expression produces a value, while a statement performs an action.
+
+// let age = 25;
+// let Name = "Harsh";
+// let isStudent = true;
+// let skills = ["js","HTML"];
+// let user = { city: "Bhopal" };
+// let x = null;
+// let y;
+// let z = Symbol("id");
+
+// console.log(typeof age); // number
+// console.log(typeof Name); //string
+// console.log(typeof isStudent); //boolean
+// console.log(typeof skills); //object
+// console.log(typeof user); //object
+// console.log(typeof x); //object
+// console.log(typeof y); //undefined
+// console.log(typeof z); //symbol
+
+
+// console.log(1/0); //infinty
+// console.log(0/0); //NaN
+// console.log(Number("abc")); //NaN
+// console.log(undefined + 1); //NaN
+
+//when you know that there will be value in future but you dont know currently so you can give null to that
+//undefined is built in 
+
+
+// let x = 5; let y = x; y = 10; console.log(x, y);
+
+
+
+
+
+
---
+++ js day 29/index.js
@@ -0,0 +1,55 @@
+
+// let password = 2324;
+
+// let validatepass = () => {
+//   for (let i = 3; i >= 1; i--) {
+//     let userpass = prompt("enter password: ");
+//     if(password === Number(userpass)){
+//       return true;
+//     }else{
+//       alert(`password is wrong`);
+//       alert(`you have ${i} reamining`);
+//     }
+//   }
+// }
+
+// if(validatepass() === true){
+//   alert(`login succesfull`);
+// }else{
+//   alert(`3 attempts finished, login blocked`);
+// }
+
+
+// let count = 0;
+// let word = null;
+
+// let wordcount = () => {
+//   while (word !== "stop") {
+//     word = prompt("enter word");
+//     if (word === "stop") {
+//       return count;
+//     }
+//     count++;
+//   }
+// }
+
+
+// console.log(wordcount());
+
+// let num = null;
+
+// while(Number(num) % 2 == 0){
+//   num = prompt("enter number");
+//   console.log(num);  
+// }
+
+// let start = prompt("enter number");
+// let end = prompt("enter number");
+
+// if(Number(start) !== NaN && Number(end) !== NaN && Number(start) >= 0){
+//   for(let i = Number(start); i <= Number(end); i++){
+//     console.log(i);
+//   }
+// }else{
+//   console.log("invalid number"); 
+// }
